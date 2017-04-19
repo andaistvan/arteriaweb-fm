06b1805d79d8a5c23b25d0885a05375efa26c413
andaistvan <andaistvan@gmail.com>
2017-04-19 17:35:01 +0200

    3.
    bj,vjvh

---
+++ assets/js/theme-init-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".dropdown-button").dropdown({inDuration:300,outDuration:225,constrainWidth:!1,hover:!0,gutter:0,belowOrigin:!0,alignment:"left",stopPropagation:!0})});
